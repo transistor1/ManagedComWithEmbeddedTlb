ccae2ae3e1ed9651616d859a08ae087e0efce153
Steve Russo <svrusso1@gmail.com>
2016-12-17 23:49:52 -0500

    Initial commit

---
+++ ComWithEmbeddedTypeLib/ComWithEmbeddedTypeLib.cs
@@ -0,0 +1,25 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComLib
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class ComWithEmbeddedTypeLib : IComWithEmbeddedTypeLib
+    {
+        [DllExport("CreateObject", CallingConvention = CallingConvention.StdCall)]
+        public static IComWithEmbeddedTypeLib CreateObject()
+        {
+            return new ComWithEmbeddedTypeLib();
+        }
+        public string HelloWorld(string name)
+        {
+            return String.Format("Hello, {0}!", name);
+        }
+    }
+}
